6df98ae7d71a033bb5a1009d6acf065f492374d3
Advait Junnarkar <advaitju@users.noreply.github.com>
2017-03-06 16:32:51 +1100

    Initial commit - including RoR files.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def index
+    @images = ["http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg",
+    "https://pbs.twimg.com/profile_images/715620604498436098/LW9o64IM.jpg",
+    "http://www.newyorker.com/wp-content/uploads/2016/05/04222016-Cartoon-Lounge-1200-630.jpg",
+    "http://www.palmbeachpost.com/rf/image/Pub/p7/CmgSharedContent/2016/02/29/Images/photos.medleyphoto.8848927.jpg",
+    "http://www.telegraph.co.uk/content/dam/opinion/2016/03/30/trump1-xlarge_trans_NvBQzQNjv4BqpJliwavx4coWFCaEkEsb3kvxIt-lGGWCWqwLa_RXJU8.jpg",
+    "https://thedodolive-img.rbl.ms/simage/https%3A%2F%2Fassets.rbl.ms%2F1569172%2F1216x700.jpg/2000,2000/ZPcHhzop%2F8%2BtxQm5/img.jpg",
+    "https://thumbs.dreamstime.com/z/donald-trump-president-cartoon-illustration-showing-head-american-real-estate-magnate-television-personality-politician-63095349.jpg",
+    "https://s-media-cache-ak0.pinimg.com/736x/03/1d/0f/031d0f30b36713ba42ea6f04f9114b31.jpg"]
+  end
+end
